b2488bf63b697d17951fa06ad0e205c4dd58c174
Panda <52302733+panda1835@users.noreply.github.com>
2021-03-21 22:22:13 +0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,18 @@
+class Model:
+	def __init__(self):
+		'''
+		Constructor 
+		'''
+		pass
+
+	def mealplan(self, 
+               free_time_to_cook,
+							 nutritious_restriction,
+               recipes_list,
+               precooked_meals,
+               fixed_meals):
+
+		p
+  
+  	def other_logic(self):
+		pass
