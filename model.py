4f138a0b7b1ec05dbba0bd24d658fe874217a23f
Panda <52302733+panda1835@users.noreply.github.com>
2021-03-22 09:22:48 +0700

    first skeleton for BE, add update database for user_defined_meal, recipe, user_info, user_nutritious_restriction

--- model.py
+++ model.py
@@ -5,14 +5,107 @@ class Model:
 		'''
 		pass
 
-	def mealplan(self, 
-               free_time_to_cook,
-							 nutritious_restriction,
-               recipes_list,
-               precooked_meals,
-               fixed_meals):
+	def user_info_to_database(nutrition_standard,
+                           height,
+                           weight,
+                           age,
+                           meal_list):
+        """
+        Store user info from surveys to database
+        @param
+        nutrition_standard: String
+        height: Int
+        weight: Int
+        age: Int
+        meal_list: List<String>
 
-		p
-  
-  	def other_logic(self):
+        Database: UserInfo
+        @column
+        nutrition_standard: String
+        height: Int
+        weight: Int
+        age: Int
+        meal_list: List<String>
+        """
+
+        pass
+    def user_nutritious_restriction_to_database():
+        """
+        To be determined
+        """
+        pass
+
+    def user_defined_meal_to_database(name,
+                         time,
+                         set_of_dishes,
+                         nutritious_restriction,
+                         regular,
+                         flexible)
+
+        """
+        Store user defined meal to database
+        @param
+        name: String
+        time: [Int, Int, Int, Int]
+        set_of_dishes: List<String>
+        nutritious_restriction: To be determined
+        regular: Bol
+        flexible: Bol
+
+        Database: UserDefinedMeal
+        @column
+        name: String
+        start_time: String (ex "12:00")
+        end_time: String (ex "12:00")
+        set_of_dishes: String 
+        nutritious_restriction: To be determined
+        regular: Bol
+        flexible: Bol
+        """
+        
+        pass
+
+    def recipe_to_database(name,
+                           serving_size,
+                           cooking_time,
+                           tag,
+                           ingredients,
+                           nutritions,
+                           steps_taken):
+        """
+        Store new recipe to database
+        @param
+        name: String
+        serving_size: Int
+        cooking_time: Int
+        tag: String
+        ingredients: (String, Int)
+        nutritions: (String, Int)
+        steps_taken: String
+        
+        Database: Recipe
+        @column
+        name: String
+        serving_size: Int
+        cooking_time: Int
+        tag: String
+        ingredient_name: String
+        amount: Int
+        nutrition_name: String
+        energy: Int
+        steps_taken: String
+        """
+        pass 
+
+	def mealplan(self, 
+                 free_time_to_cook,
+                 nutritious_restriction,
+                 recipes_list,
+                 precooked_meals,
+                 fixed_meals):
+
+        """
+        Generate new mealplan
+        """
 		pass
+  
