8a08c67d7d9c454c4a410e4bd17c8258f86d40c7
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-04-02 16:36:02 +0700

    done storing nutrition plan, body condition and period

--- view.py
+++ view.py
@@ -49,7 +49,8 @@ class View(tk.Tk):
 
     def next(self, main_view, field, info, next_frame):
         main_view.show_frame(next_frame)
-        main_view.user_info[field] = info.get()
+        for i in range(len(field)):
+            main_view.user_info[field[i]] = info[i].get()
         print(main_view.user_info)
 class CreateNutritionPlan(tk.Frame):
     '''
@@ -71,7 +72,7 @@ class CreateNutritionPlan(tk.Frame):
         
         # the back & next buttons
         next_but = tk.Button(parent, text ="Next", bg = "Green", relief = "flat", 
-        command = lambda : main_view.next(main_view, "Plan", nutrition_plan, CreateBodyCondition))
+        command = lambda : main_view.next(main_view, ["Plan"], [nutrition_plan], CreateBodyCondition))
         
         # make all elements visible by using grid
         parent.grid(column = 0, row = 0)
@@ -92,24 +93,25 @@ class CreateBodyCondition(tk.Frame):
         title = tk.Label(parent,text = "Your body condition", font = TITLEFONT, 
         bg = "white")
 
+
+        height_info = tk.IntVar()
+        weight_info = tk.IntVar()
+        age_info = tk.IntVar()
+
         # the height-weight-age titles and entries
         height = tk.Label(parent, text = "Height", bg = "white")
-        height_entry = ttk.Entry(parent, width = 10)
+        height_entry = ttk.Entry(parent, width = 10, textvariable = height_info)
         weight = tk.Label(parent, text = "Weight", bg = "white")
-        weight_entry = ttk.Entry(parent, width = 10)
+        weight_entry = ttk.Entry(parent, width = 10, textvariable = weight_info)
         age = tk.Label(parent, text = "Age", bg = "white")
-        age_entry = ttk.Entry(parent, width = 10)
-
-        height_info = height_entry.get()
-        weight_info = weight_entry.get()
-        age_info = age_entry.get()
+        age_entry = ttk.Entry(parent, width = 10, textvariable = age_info)
 
         #the back & next buttons
         back_but = tk.Button(parent, text = "Back", bg = "Green", relief = "flat", 
         command = lambda : main_view.show_frame(CreateNutritionPlan))
         next_but = tk.Button(parent, text ="Next", bg = "Green", relief = "flat", 
         # command = lambda : main_view.show_frame(CreatePlanPeriod))
-        command = lambda : main_view.next(main_view, "Body", (height_info, weight_info, age_info), CreatePlanPeriod))
+        command = lambda : main_view.next(main_view, ["Height", "Weight", "Age"], [height_info, weight_info, age_info], CreatePlanPeriod))
 
         # make all elements visible by using grid
         parent.grid(column = 0, row = 0)
@@ -146,7 +148,7 @@ class CreatePlanPeriod(tk.Frame):
         back_but = tk.Button(parent, text = "Back", bg = "Green", relief = "flat", 
         command = lambda : main_view.show_frame(CreateBodyCondition))
         next_but = tk.Button(parent, text ="Next", bg = "Green", relief = "flat", 
-        command = lambda : main_view.show_frame(CreateUserDefinedMeal))
+        command = lambda : main_view.next(main_view, ["Period"], [plan_period], CreateUserDefinedMeal))
         
         # make all elements visible by using grid
         parent.grid(column = 0, row = 0)
@@ -207,12 +209,12 @@ class CreateSubUserDefinedMeal(tk.Frame):
         parent = tk.Frame(self, bg = "white")
         #instantiate wireframe's components and position them within the frame
 
-        del_meal = tk.Button(parent, text = "-", bg ="grey", command = self.destroy)
+        del_meal = tk.Button(parent, text = "-", bg ="white", command = self.destroy, relief = "flat")
 
-        name_label = tk.Label(parent, text = "Name", bg ="grey", anchor = "w", justify="left", pady=5, padx=5)
+        name_label = tk.Label(parent, text = "Name", bg ="white", anchor = "w", justify="left", pady=5, padx=5)
         name_entry = tk.Entry(parent)
         
-        time_label = tk.Label(parent, text = "Time", bg ="grey")
+        time_label = tk.Label(parent, text = "Time", bg ="white")
         valuelist = ["05:00","05:15","05:30","05:45","06:00","06:15","06:30","06:45","07:00","07:15","07:30","07:45",
                      "08:00","08:15","08:30","08:45","09:00","09:15","09:30","09:45","10:00","10:15","10:30","10:45",
                      "11:00","11:15","11:30","11:45","12:00","12:15","12:30","12:45","13:00","13:15","13:30","13:45",
@@ -221,7 +223,6 @@ class CreateSubUserDefinedMeal(tk.Frame):
                      "20:00","20:15","20:30","20:45","21:00","21:15","21:30","21:45","22:00","22:15","22:30","22:45"]
         start_time   = tk.StringVar()
         end_time     = tk.StringVar()
-
         time_start_combo  = ttk.Combobox(parent, width = 5, values = valuelist, textvariable = start_time)
         time_end_combo    = ttk.Combobox(parent, width = 5, values = valuelist, textvariable = end_time)
         dash_label        = tk.Label(parent, text = "-")
@@ -231,17 +232,19 @@ class CreateSubUserDefinedMeal(tk.Frame):
         if isinstance(start_time, str):
             print(start_time) 
 
-        set_dish_label    = tk.Label(parent, text = "Set of dishes", bg ="grey")
+        dish_label = tk.StringVar()
+        set_dish_label    = tk.Label(parent, text = "Set of dishes", bg ="white")
         set_dish_entry    = tk.Entry(parent)
-
-        nutri_label       = tk.Label(parent, text = "Nutritional restriction", bg ="grey")
+        
+        nutri_value = tk.StringVar()
+        nutri_label       = tk.Label(parent, text = "Nutritional restriction", bg ="white")
         nutri_entry       = tk.Entry(parent)
         
         #variable to keep track the checkbutton
         reg = tk.IntVar()
         flex = tk.IntVar()
-        regular  = tk.Checkbutton(parent, text ="Regular", variable = reg, bg ="grey")
-        flexible = tk.Checkbutton(parent, text ="Flexible", variable = flex, bg ="grey")
+        regular  = tk.Checkbutton(parent, text ="Regular", variable = reg, bg ="white")
+        flexible = tk.Checkbutton(parent, text ="Flexible", variable = flex, bg ="white")
     
         # make all elements visible by using grid
         parent.grid(column = 0, row = 0)
