dadc37d4c9ec8501cc65282f7a00cc19786f8263
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-04-02 17:50:01 +0700

    add controller to call model save user info

--- controller.py
+++ controller.py
@@ -1,14 +1,2 @@
 from model import Model
 from view import View
-
-class Controller:
-
-	def __init__(self):
-		'''
-		Constructor 
-		'''
-		self.model = Model()
-		self.view = View(self)
-
-	def main(self):
-		self.view.main() 
