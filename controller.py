f8fe25cd06594c9c9311ab8b6f1e2951be826539
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-05-05 18:38:18 +0700

    add save recipe

--- controller.py
+++ controller.py
@@ -35,3 +35,14 @@ class Controller:
             defined_meal['flexible'] = meal.flex.get()
             list_defined_meal.append(defined_meal)
         Model.set_user_defined_meal(list_defined_meal)
+
+    def save_recipe(self, recipe_object):
+        recipe = {}
+        recipe['recipe_name'] = recipe_object.recipe_name
+        recipe['serving_size'] = recipe_object.serving_size
+        recipe['cooking_time'] = recipe_object.cooking_time
+        recipe['tag'] = recipe_object.tag
+        recipe['ingredients'] = recipe_object.ingredients
+        recipe['nutritions'] = recipe_object.nutritions
+        recipe['steps_taken'] = recipe_object.steps_taken
+        Model.set_recipe(recipe)
