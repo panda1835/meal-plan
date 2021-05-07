33171f07497dc008c3c054ad758dd2f7ee7cc3c2
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-05-07 20:22:32 +0700

    edit save database API

--- PlanPeriod.py
+++ PlanPeriod.py
@@ -37,9 +37,25 @@ class PlanPeriod(tk.Frame):
         savebtn.grid(column = 2, row = 3, padx = 15, pady = 10)
     
     def save_database(self, main_view):
-        main_view.controller.save_user_info(main_view.user_info)
-        # print(main_view.controller.get_user_info())
-        main_view.controller.save_user_defined_meal(main_view.user_defined_meal)
-        # print(main_view.controller.get_user_defined_meal())
+        info = {'nutrition_standard': "", 'height': 0, 'weight': 0, 'age': 0,'plan_period': "weekly"}
+        info['nutrition_standard'] = main_view.user_info["Nutrition"].nutrition_plan.get()
+        info['height'] = main_view.user_info["Body"].height_info.get()
+        info['weight'] = main_view.user_info["Body"].weight_info.get()
+        info['age'] = main_view.user_info["Body"].age_info.get()
+        info['plan_period]'] = main_view.user_info["Plan"].plan_period.get()
+        main_view.controller.save_user_info(info)
+        
+        list_defined_meal = []
+        for meal in main_view.user_defined_meal:
+            defined_meal = {}
+            defined_meal['meal_name'] = meal.name.get()
+            defined_meal['start_time'] = meal.start_time.get()
+            defined_meal['end_time'] = meal.end_time.get()
+            defined_meal['set_of_dishes'] = meal.set_of_dishes.get()
+            defined_meal['nutritious_restriction'] = meal.nutritious_restriction.get()
+            defined_meal['regular'] = meal.reg.get()
+            defined_meal['flexible'] = meal.flex.get()
+            list_defined_meal.append(defined_meal)
+        main_view.controller.save_user_defined_meal(list_defined_meal)
         main_view.show_frame(main_view.frame_Recipe)
         main_view.show_frame(main_view.frame_MainUI)
