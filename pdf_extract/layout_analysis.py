58c44330d0d397766f537f7a871f431bbcae2824
unknown <phuthienng@gmail.com>
2021-05-13 15:57:58 +0700

    Done with creating database

--- pdf_extract/layout_analysis.py
+++ pdf_extract/layout_analysis.py
@@ -3,40 +3,22 @@ from pdfminer.converter import PDFPageAggregator
 from pdfminer.pdfpage import PDFPage
 from pdfminer.layout import LTTextBoxHorizontal
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-
-#####################################################################
 import re 
 import sqlite3 
-#from layout_analysis import nutrient_list 
 
 connection = sqlite3.connect('nutrient_data.db')
 cursor = connection.cursor()
 
 raw_NutrientData = '''   
-    Food 
-    Nước (Water ) 
-    Năng lượng (Energy ) 
-    Protein 
-    Lipid (Fat) 
-    Glucid (Carbohydrate) 
-    Celluloza (Fiber) 
-    Tro (Ash) 
-    Đường tổng số (Sugar) 
-    Galactoza (Galactose) 
-    Maltoza (Maltose)
-    Lactoza (Lactose) 
-    Fructoza  (Fructose) 
-    Glucoza (Glucose) 
-    Sacaroza (Sucrose) 
+    Food
     Calci (Calcium) 
-    Sắt (Iron) 
-    Magiê (Magnesium)
+    Sat (Iron) 
+    Magie (Magnesium)
     Mangan (Manganese) 
     Phospho (Phosphorous)
-    Kali (Potassium)
     Natri (Sodium) 
-    Kẽm  (Zinc) 
-    Đồng (Copper) 
+    Kem (Zinc) 
+    Dong (Copper) 
     Selen (Selenium) 
     Vitamin C (Ascorbic acid)
     Vitamin B1 (Thiamine)
@@ -45,27 +27,21 @@ raw_NutrientData = '''
     Vitamin B5 (Pantothenic acid) 
     Vitamin B6 (Pyridoxine) 
     Folat (Folate) 
-    Vitamin B9 (Folic acid) 
     Vitamin H (Biotin ) 
     Vitamin B12 (Cyanocobalamine) 
     Vitamin A (Retinol) 
     Vitamin D (Calciferol ) 
     Vitamin E  (Alpha-tocopherol )
     Vitamin K (Phylloquinone) 
-    Beta_caroten 
-    Alpha_caroten 
-    Beta_cryptoxanthin 
-    Lutein_Zeaxanthin 
-    Lycopen 
-    Purin '''
+'''
 
 string_NutrientData = ""
 for element in raw_NutrientData.split("\n"): 
     if element.strip() != '': 
         element = element.replace(" ", "")
         element = re.sub(r"\([^()]*\)", "", element)
-        string_NutrientData += element + " TEXT" + ", "
-string_NutrientData = string_NutrientData.rstrip(", ") 
+        string_NutrientData += element + " TEXT" + ","
+string_NutrientData = string_NutrientData.rstrip(",") 
 string_NutrientData = "NutrientData(" + string_NutrientData + ")"
 
 string_NutrientCommand = string_NutrientData.replace("TEXT", "")
@@ -85,75 +61,93 @@ def insert_data(food, food_list):
 
 def test_insert(): 
     cursor.execute("SELECT * FROM NutrientData")
-    data = cursor.fetchall() 
+    data = cursor.fetchone() 
     connection.commit()
     print(data)
 
 def process_string(word): 
     if '\n' in word: 
-        correct_word = word.split("\n")[0]
-        return correct_word 
-    elif re.search("[^0-9|^.]", word):
-        correct_word = None
-        return correct_word
+        word = word.split("\n")[0]
+    
+    if re.search("[^0-9|^.]", word):
+        word = None
 
+    return word 
 
-#####################################################################
-
-
-box_general = [] 
-box_sugar = [] 
-box_micronutrients = [] 
-
-document = open('G:/Programming/Meal-Plan/pdf_extract/demo_page_2.pdf', 'rb')
-#Create resource manager
-rsrcmgr = PDFResourceManager()
-# Set parameters for analysis.
-laparams = LAParams()
-# Create a PDF page aggregator object.
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-interpreter = PDFPageInterpreter(rsrcmgr, device)
- 
-
-for page in PDFPage.get_pages(document):
-    box_general = [] 
-    box_sugar = [] 
+def create_database(): 
     box_micronutrients = [] 
 
-    interpreter.process_page(page)
-    # receive the LTPage object for the page.
-    layout = device.get_result()
-    i = 0 
-    nutri_index = 0 
-    nutri_index_2 = 0 
+    document = open('G:/Programming/Meal-Plan/pdf_extract/group_4.pdf', 'rb')
+    #Create resource manager
+    rsrcmgr = PDFResourceManager()
+    # Set parameters for analysis.
+    laparams = LAParams()
+    # Create a PDF page aggregator object.
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    
 
-    for element in layout: 
-        i+= 1 
-        if i == 3: 
-            title = element.get_text().rstrip('\n')
-            title = process_string(title)
+    for page in PDFPage.get_pages(document):
+        # box_general = [] 
+        # box_sugar = [] 
+        box_micronutrients = [] 
 
-        if i in range(109, 117): 
-            word = element.get_text().rstrip('\n')
-            word = process_string(word)
-            box_general.append(word)
+        interpreter.process_page(page)
+        # receive the LTPage object for the page.
+        layout = device.get_result()
+        i = 0 
+        nutri_index = 0 
+        nutri_index_2 = 0 
 
-        if i in range(133, 140): 
-            word = element.get_text().rstrip('\n')
-            word = process_string(word)
-            box_sugar.append(word)
+        for element in layout: 
+            i+= 1 
+            
+            if i == 4: 
+                title = element.get_text().rstrip('\n')
+                print(title)
 
-        if i in range(117, 133): 
-            word = element.get_text().rstrip('\n')
-            word = process_string(word)
-            box_micronutrients.append(word)
+            if i in range(117, 133) and i != 122:  
+                word = element.get_text().rstrip('\n')
+                word = process_string(word)
+                box_micronutrients.append(word)
 
-        if i in range(140, 153): 
-            word = element.get_text().rstrip('\n')
-            word = process_string(word)
-            box_micronutrients.append(word)
+            if i in range(140, 148) and i != 141: 
+                word = element.get_text().rstrip('\n')
+                word = process_string(word)
+                box_micronutrients.append(word)
 
-    nutrient_list = box_general + box_sugar + box_micronutrients
-    insert_data(title, nutrient_list)
+        nutrient_list = box_micronutrients
+        
+        
+        insert_data(title, nutrient_list)
+    
+def get_nutrition(meal, nutrition): 
+    connection = sqlite3.connect('nutrient_data.db')
+    cursor = connection.cursor() 
 
-test_insert() 
+    cursor.execute('''SELECT * FROM NutrientData
+                        WHERE Food = ?''', [meal])
+    data = cursor.fetchall()
+    connection.commit() 
+    if nutrition == "Calci": return data[1]
+    if nutrition == "Sat": return data[2] 
+    if nutrition == "Magie": return data[3]  
+    if nutrition == "Mangan": return data[4]  
+    if nutrition == "Phospho": return data[5]
+    if nutrition == "Natri": return data[4] 
+    if nutrition == "Kem": return data[5]
+    if nutrition == "Dong": return data[6]
+    if nutrition == "Selen": return data[7]
+    if nutrition == "VitaminC": return data[8]
+    if nutrition == "VitaminB1": return data[9] 
+    if nutrition == "VitaminB2": return data[10]
+    if nutrition == "VitaminPP": return data[11]  
+    if nutrition == "VitaminB5": return data[12] 
+    if nutrition == "VitaminB6": return data[13]
+    if nutrition == "Folat": return data[14]
+    if nutrition == "VitaminH": return data[15]
+    if nutrition == "VitaminB12": return data[16]
+    if nutrition == "VitaminA": return data[17]
+    if nutrition == "VitaminD": return data[18]
+    if nutrition == "VitaminE": return data[19] 
+    if nutrition == "VitaminK": return data[20]
--- pdf_extract/split_page.py
+++ pdf_extract/split_page.py
@@ -7,46 +7,11 @@ print('Number of pages: ' + str(num_of_pages))
 
 writer = PyPDF2.PdfFileWriter() # writing PDF File out
 
-for page in range(14, 17):  
+for page in range(231, 286):  
     writer.addPage(reader.getPage(page))
 
-output_filename = 'G:/Programming/Meal-Plan/pdf_extract/demo_page_2.pdf'
+output_filename = 'G:/Programming/Meal-Plan/pdf_extract/group_5.pdf'
 
 with open(output_filename, 'wb') as output: 
     writer.write(output) 
 
-# text = data_func.convert_pdf_to_string('G:/Programming/Meal-Plan/pdf_extract/demo_page.pdf')
-# text = text.replace('.', '')
-# text = text.replace('\x0c', '')
-# table_of_contents_raw = text.split('\n')
-# for line in table_of_contents_raw: 
-#     print(line.strip())
-
-# print(" *********************TEXT****************")
-# print(text)
-# print("**********************TABLE OF CONTEXT***************")
-# print(table_of_contents_raw)
-
-"""
-title_list = [] 
-pagenum_list = [] 
-title_formatted_list = [] 
-
-for item in table_of_contents_raw:
-    title, pagenum =  data_func.split_to_title_and_pagenum(item)
-    if title != None:
-        title_list.append(title)
-        pagenum_list.append(pagenum)
-        title_formatted_list.append(
-            data_func.convert_title_to_filename(title))
-    
-# for page_list, we need to add the last page as well
-pagenum_list.append(num_of_pages + 1)
-
-print("*******TITLE LIST*********")
-print(title_list)
-print("*******PAGENUM LIST*********")
-print(pagenum_list)
-print("***********TITLE FORMATTED LIST************")
-print(title_formatted_list)
-"""
